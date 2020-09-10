64a9924d32746f2bb09182817f7745a284843773
Dmitry Spitsyn <70942347+dspitsyn@users.noreply.github.com>
2020-09-10 12:35:54 +0200

    V.2004.182804
    Securing SQL Server

--- CIS/Securing_SQL_Server_Solution.sql
+++ CIS/Securing_SQL_Server_Solution.sql
@@ -1,13 +1,13 @@
 /*
   Version:	2004.182804 - April, 2020
   Summary:
-		This solution was created partly based on https://www.cisecurity.org (State: 31 July, 2019).
-		Allows to completely lock down a server, except for the particular services that it requires to perform specific duties.
-		Some of the features and services, provided by default, may not be necessary, and enabling them could adversely affect the security of the system.
-		You should understand all hardening techniques available so that you can determine which hardening strategies work best for your organization. 
-		Make sure that not every hardening technique works for everyone.
-		Balancing operational consistency and performance monitoring is imperative for large SQL Server environments.
-		Following solution introduced in order for SQL Server DBAs to streamline, prioritize, and simplify SQL Server monitoring.
+	This solution was created partly based on https://www.cisecurity.org (State: 31 July, 2019).
+	Allows to completely lock down a server, except for the particular services that it requires to perform specific duties.
+	Some of the features and services, provided by default, may not be necessary, and enabling them could adversely affect the security of the system.
+	You should understand all hardening techniques available so that you can determine which hardening strategies work best for your organization. 
+	Make sure that not every hardening technique works for everyone.
+	Balancing operational consistency and performance monitoring is imperative for large SQL Server environments.
+	Following solution introduced in order for SQL Server DBAs to streamline, prioritize, and simplify SQL Server monitoring.
 
   Version Updates:
 	29.01.2020: | Added by Dmitry Spitsyn
@@ -83,7 +83,7 @@
 		- several "Authentication and Authorization" topics
 		- Orphaned Users
 		- Encryption
-		Common Bayer Solution also discussed in ICS Audit reagarding SQL CCS Development/Integration and SQL Exception Solution.
+		Common Solution also discussed in ICS Audit reagarding SQL CCS Development/Integration and SQL Exception Solution.
 	--||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 	01.08.2019: | Added by Dmitry Spitsyn
 		# New queries structure 

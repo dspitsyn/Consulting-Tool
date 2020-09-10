c25b1fcfc3a9480758b54cadf6906699bd9e7083
Dmitry Spitsyn <70942347+dspitsyn@users.noreply.github.com>
2020-09-10 12:29:36 +0200

    V.16.01
    Moving tempdb data files

--- Scripts/Moving_TempDB.sql
+++ Scripts/Moving_TempDB.sql
@@ -1,10 +1,11 @@
 /*
+  | Version: 16.01
+  | Description:
   | These days, 8 files is where most folks see performance level off, if 4 isn't cutting it.
   | Just make sure they're all the same starting size and have all the same autogrowth settings. 
   | Having unevenly sized files can lead to uneven query distribution across them.
-
 */
-SELECT name, physical_name AS CurrentLocation  
+SELECT name, physical_name AS [CurrentLocation]
 FROM sys.master_files  
 WHERE database_id = DB_ID(N'tempdb');  
 GO

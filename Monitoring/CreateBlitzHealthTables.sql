c085485df2d261431ebea958a2c8c7fba1dabd08
Dmitry Spitsyn <70942347+dspitsyn@users.noreply.github.com>
2020-10-05 16:43:50 +0200

    CreateBlitzHealthTables

---
+++ Monitoring/CreateBlitzHealthTables.sql
@@ -0,0 +1,35 @@
+USE [USAGE]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MemoryPressure](
+	[ID] [int] NULL,
+	[Counter Name] [nvarchar](128) NULL,
+	[Value] [numeric](20, 2) NULL
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[MemoryAllocation](
+	[PhysicalMemoryUsedBySQL] [bigint] NULL,
+	[PhysicalMemoryLow] [bit] NULL,
+	[VirtualMemoryLow] [bit] NULL,
+	[EventTime] [datetime] NULL
+) ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[LogArchive](
+	[LogArchiveID] [int] IDENTITY(1,1) NOT NULL,
+	[LogDate] [datetime] NULL,
+	[ProcessInfo] [varchar](50) NULL,
+	[Text] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+CREATE TABLE [dbo].[ErrorArchiveCurrent](
+	[ErrorID] [int] IDENTITY(1,1) NOT NULL,
+	[ErrorDate] [datetime] NULL,
+	[ProcessInfo] [varchar](50) NULL,
+	[Text] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
